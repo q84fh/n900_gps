1a3ce0b590ed2f789158e1767e098b45f4995bf2
Jakub Skory <git@q84fh.net>
2017-03-17 23:45:21 +0100

    Current state

--- gps.py
+++ gps.py
@@ -7,7 +7,9 @@ import gobject
 from datetime import datetime
 import sqlite3 as db
 from subprocess import Popen
+from subprocess import call
 from pythonwifi import iwlibs
+import time
 
 def log(str):
     t = datetime.now()
@@ -21,6 +23,114 @@ def check_pid(pid):
       return False
    else:
       return True
+
+def on_error(control, error, data):
+    print "location error: %d... quitting" % error
+    data.quit()
+
+def check_fix(device):
+  if not device:
+    log("There is no GPS device.")
+    return False
+  if not device.fix:
+    log("Device has not fix.")
+    return False
+  if device.satellites_in_use == 0:
+    log("0 sattelites in use.")
+    return False
+  if not device.fix[1] & location.GPS_DEVICE_LATLONG_SET:
+    log("Device does not have latlong set.")
+    return False
+  if not device.fix[1] & location.GPS_DEVICE_TIME_SET:
+    log("Device does not have time set.")
+    return False
+  if device.status & location.GPS_DEVICE_STATUS_NO_FIX:
+    log("Device has status no_fix.")
+    return False
+  return True
+
+def on_changed(device, data):
+    global has_fix
+    global poczatek
+    global first
+    global p
+    global first
+    global count
+    pomiar_id = False
+    delta = datetime.now() - poczatek  
+    if delta.seconds > 780:
+      log("No fix found in more then 600 sec")
+      data.stop()
+
+    if check_fix(device):
+      if len(sys.argv) > 1 and count > int(sys.argv[1]):
+          log("Count exceeded, exitting.")
+          data.stop()
+      count = count + 1
+      if not has_fix:
+          log("Fix acquired.")
+          has_fix = True
+      wifi = iwlibs.Wireless("wlan0")
+      aq = wifi.getStatistics()[1]
+      Siglevel = aq.siglevel
+      Nlevel = aq.nlevel
+      Quality = aq.quality
+      
+      poczatek = datetime.now()
+      cur = con.cursor()    
+      cur.execute("INSERT INTO POMIARY (mode,fields,time,ept,latitude,longitude,eph,altitude,epv,track,epd,speed,eps,climb,epc,sat_seen,sat_used,apaddr,essid,siglevel,nlevel,quality) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",(device.fix[0],device.fix[1],device.fix[2],device.fix[3],device.fix[4],device.fix[5],device.fix[6],device.fix[7],device.fix[8],device.fix[9],device.fix[10],device.fix[11],device.fix[12],device.fix[13],device.fix[14],device.satellites_in_view,device.satellites_in_use,wifi.getAPaddr(),wifi.getEssid(),Siglevel,Nlevel,Quality))
+      con.commit()
+      cur.execute("SELECT last_insert_rowid() AS LAST")
+      pomiar_id = cur.fetchone()[0] 
+      #if first or not p.poll():
+      #    first = 0
+          #p = Popen(['/root/bin/gps_send.py'])
+    else:
+       if has_fix:
+          log('Fix lost')
+          has_fix = False
+    if geolocate_wifi(pomiar_id):
+      data.stop()
+
+def on_stop(control, data):
+    log("Stopping GPS...")
+    data.quit()
+
+def start_location(data):
+    data.start()
+    return False
+
+def geolocate_wifi(pomiar_id=False):
+    best_bssid = "00:00:00:00:00:00"
+    best_quality = 0
+    call(['/sbin/ifconfig', 'wlan0', 'up'])
+    i = 0
+    for siec in iwlibs.Iwscan("wlan0",fullscan = True):
+      if pomiar_id:
+        cur = con.cursor()
+        i = i + 1   
+        cur.execute("INSERT INTO sieci(ssid,bssid,siglevel,nlevel,quality,pomiar_gps) VALUES (?,?,?,?,?,?)",(siec.essid,siec.bssid,siec.quality.siglevel,siec.quality.nlevel,siec.quality.quality,pomiar_id))
+        con.commit()
+
+      if best_quality < siec.quality.quality:
+        cur = con.cursor()
+        cur.execute("select * from znane_sieci WHERE bssid = ?;",[siec.bssid])
+        wynik = cur.fetchone()
+        if(wynik):
+          best_bssid = siec.bssid
+          best_quality = siec.quality.quality
+          best_wifi_coord = (wynik['lat'],wynik['long'],wynik['delta'])
+          best_siec = siec
+
+    if i > 0:
+        log("WiFi found: %s " % i)  
+    if best_bssid != "00:00:00:00:00:00":
+        log("Known WiFi found: %s..." % best_siec.essid)
+        cur.execute("INSERT INTO POMIARY (mode,latitude,longitude,eph,apaddr,essid,siglevel,nlevel,quality) VALUES (3,?,?,?,?,?,?,?,?)", (best_wifi_coord[0],best_wifi_coord[1],best_wifi_coord[2],best_siec.bssid,best_siec.essid,best_siec.quality.siglevel,best_siec.quality.nlevel,best_siec.quality.quality))
+        con.commit()
+        #p = Popen(['/root/bin/gps_send.py'])
+        return True
+    return False
 
 pid = str(os.getpid())
 pidfile = "/tmp/gps.pid"
@@ -36,109 +146,47 @@ if os.path.isfile(pidfile):
 else:
     file(pidfile, 'w').write(pid)
 
-def on_error(control, error, data):
-    print "location error: %d... quitting" % error
-    data.quit()
 
-def on_changed(device, data):
-    global poczatek
-    global first
-    global p
-    global first
-    global count
-#   Record = namedtuple("fix", ["mode","fields","time","ept","latitude","longitude","eph","altitude","epv","track","epd","speed","eps","climb","epc"])
-#   data = Record(*device.fix)
-    delta = datetime.now() - poczatek
-    log("Szukam od %d (%d min.) Slysze %d satelit, w zasiegu mam %d" % (delta.seconds, delta.seconds/60,device.satellites_in_view,device.satellites_in_use))
-    if delta.seconds > 780:
-        log("No fix found in more then 600 sec")
-        data.stop()
-    if not device:
-        return
-    if device.fix:
-        if (device.fix[1] & 
-location.GPS_DEVICE_LATLONG_SET) and (device.fix[1] & 
-location.GPS_DEVICE_TIME_SET) and not (device.status 
-& location.GPS_DEVICE_STATUS_NO_FIX):
-            if len(sys.argv) > 1 and count > int(sys.argv[1]):
-                log("Count exceeded, exitting.")
-                data.stop()
-            count = count + 1
-            log("Fix: %f, %f" % device.fix[4:6])
-            
-            wifi = iwlibs.Wireless("wlan0")
-            APaddr = wifi.getAPaddr()
-            Essid = wifi.getEssid()
-            aq = wifi.getStatistics()[1]
-            Siglevel = aq.siglevel
-            Nlevel = aq.nlevel
-            Quality = aq.quality
-            
-            
-            poczatek = datetime.now()
-            cur = con.cursor()    
-            cur.execute("INSERT INTO POMIARY (mode,fields,time,ept,latitude,longitude,eph,altitude,epv,track,epd,speed,eps,climb,epc,sat_seen,sat_used,apaddr,essid,siglevel,nlevel,quality) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",(device.fix[0],device.fix[1],device.fix[2],device.fix[3],device.fix[4],device.fix[5],device.fix[6],device.fix[7],device.fix[8],device.fix[9],device.fix[10],device.fix[11],device.fix[12],device.fix[13],device.fix[14],device.satellites_in_view,device.satellites_in_use,APaddr,Essid,Siglevel,Nlevel,Quality))
-            con.commit()
-            cur.execute("SELECT last_insert_rowid() AS LAST")
-            pomiar_id = cur.fetchone()[0] 
-            #for siec in iwlibs.Iwscan("wlan0",fullscan=True)
-            #   cur.execute("INSERT INTO SIECI (ssid,bssid,siglevel,nlevel,quality,pomiar_gps) VALUES (?,?,?,?,?,?)",(siec.essid,siec.bssid,siec.quality.siglevel,siec.quality.nlevel,siec.quality.quality,pomiar_id))
-            #   con.commit()
-            if first or not p.poll():
-                first = 0
-                p = Popen(['/root/bin/gps_send.py'])
 
-def on_stop(control, data):
-    #print ""
-    os.unlink(pidfile)
-    data.quit()
-
-def start_location(data):
-    data.start()
-    return False
-
-signal.signal(signal.SIGINT,on_stop)
-first = 1
-count = 0
-
-con = db.connect('/root/bin/gps.db')
+con = db.connect('/home/user/MyDocs/gps.db')
 con.row_factory = db.Row
-wifi_first = iwlibs.Wireless("wlan0")
-APaddr_first = wifi_first.getAPaddr()
-if(APaddr_first != "00:00:00:00:00:00"):
-   cur = con.cursor()
-   cur.execute("select * from znane_sieci WHERE bssid = ?;",[APaddr_first])
-   wynik = cur.fetchone()
-   if(wynik):
-      log("Znana sieci wifi")
-      wifi = iwlibs.Wireless("wlan0")
-      APaddr = wifi.getAPaddr()
-      Essid = wifi.getEssid()
-      aq = wifi.getStatistics()[1]
-      Siglevel = aq.siglevel
-      Nlevel = aq.nlevel
-      Quality = aq.quality
-      cur.execute("INSERT INTO POMIARY (mode,latitude,longitude,eph,apaddr,essid,siglevel,nlevel,quality) VALUES (3,?,?,?,?,?,?,?,?)",(wynik['lat'],wynik['long'],wynik['delta'],APaddr,Essid,Siglevel,Nlevel,Quality))
-      con.commit()
-      p = Popen(['/root/bin/gps_send.py'])
-      print wynik
-      os.unlink(pidfile)
-      sys.exit()
-else:
-   log("Nie polaczony z wifi, wlaczam GPS.")
 
-poczatek = datetime.now()
-loop = gobject.MainLoop()
-control = location.GPSDControl.get_default()
-device = location.GPSDevice()
-control.set_properties(preferred_method=location.METHOD_USER_SELECTED,
-                       
-preferred_interval=location.INTERVAL_DEFAULT)
+i = 1
+not_found_counter = 0
 
-control.connect("error-verbose", on_error, loop)
-device.connect("changed", on_changed, control)
-control.connect("gpsd-stopped", on_stop, loop)
+while True:
+  if not geolocate_wifi():
+    not_found_counter = not_found_counter + 1
+    if not_found_counter < 10:
+       log("No known wifi found, trying again in 3 sec (%s/10) before firing gps recivier" % (not_found_counter))
+       time.sleep(3)
+       continue
+    i = 1
+    log('Starting gps...')
 
-gobject.idle_add(start_location, control)
-   
-loop.run()
+    signal.signal(signal.SIGINT,on_stop)
+    first = 1
+    count = 0
+    has_fix = False
+    poczatek = datetime.now()
+
+
+    loop = gobject.MainLoop()
+    control = location.GPSDControl.get_default()
+    device = location.GPSDevice()
+    control.set_properties(preferred_method=location.METHOD_USER_SELECTED,
+
+    preferred_interval=location.INTERVAL_DEFAULT)
+
+    control.connect("error-verbose", on_error, loop)
+    device.connect("changed", on_changed, control)
+    control.connect("gpsd-stopped", on_stop, loop)
+
+    gobject.idle_add(start_location, control)
+    loop.run()
+  else: 
+    not_found_counter = 0
+  if i < 300:
+    i = i + 1
+  log("Sleeping... %s sec" % i)
+  time.sleep(i)
--- gps_send.py
+++ gps_send.py
@@ -4,7 +4,9 @@ import os
 import sys
 import sqlite3 as db
 import httplib, urllib
+from time import time,sleep
 from datetime import datetime
+#import socket
 
 def log(str):
     t = datetime.now()
@@ -24,7 +26,6 @@ pidfile = "/tmp/gps_send.pid"
 
 if os.path.isfile(pidfile):
     if(check_pid(int(file(pidfile, 'r').read()))):
-       log("Lock file %s already exists, exiting" % pidfile)
        sys.exit()
     else:
        log("%s stale lock file, removing" % pidfile)
@@ -33,61 +34,128 @@ if os.path.isfile(pidfile):
 else:
     file(pidfile, 'w').write(pid)
 
-con = db.connect('/root/bin/gps.db')
+con = db.connect('/home/user/MyDocs/gps.db')
 
 con.row_factory = db.Row
+con.text_factory = str
 
 cur = con.cursor() 
-
 cur.execute("select count(*) as count from POMIARY WHERE SENT = 0 AND MODE = 3")
 count = cur.fetchone()
-
-log("Starting gps_send: Entries in queue: %d" % (count['count']))
+cnt = int(count['count'])
+new_count = cnt
+time_mark = time()
+if count['count'] == 0:
+   log("Nothing to send, quiting. %d")
+   sys.exit()
+log("Starting, queue: %d" % (count['count']))
 
 cur.execute("select * from POMIARY WHERE SENT = 0 AND MODE = 3 LIMIT 1000")
 send_counter = 0
+net_counter = 0
 rows = cur.fetchall()
 headers = {"Content-type": "application/x-www-form-urlencoded",
             "Accept": "text/plain"}
-try: 
+try:
     conn = httplib.HTTPConnection("q84fh.mydevil.net")
+    
     for row in rows:
-        params = urllib.urlencode({
-            'lat': row['latitude'],
-            'long': row['longitude'],
-            'eph': row['eph'], 
-            'alt': row['altitude'],
-            'epv': row['epv'],
-            'track': row['track'],
-            'ept': row['ept'],
-            'speed': row['speed'],
-            'eps': row['eps'],
-            'climb': row['climb'],
-            'epc': row['epc'],
-            'ssid': row['essid'],
-            'bssid': row['apaddr'],
-            'signal': row['siglevel'],
-            'noise': row['nlevel'],
-            'quality': row['quality'],
-            'timestamp': row['time_aquired']
-        })
+        cur.execute("select * from sieci where pomiar_gps = ?",[row['id']])
+        sieci = cur.fetchall()
+        scn = []
+        for siec in sieci:
+          scn.append("%s;%s;%s;%s;%s" % (siec['ssid'],siec['bssid'],siec['siglevel'],siec['nlevel'],siec['quality']))
+          net_counter = net_counter + 1
+        try:
+         params = urllib.urlencode({
+             'lat': row['latitude'],
+             'long': row['longitude'],
+             'eph': row['eph'], 
+             'alt': row['altitude'],
+             'epv': row['epv'],
+             'track': row['track'],
+             'ept': row['ept'],
+             'speed': row['speed'],
+             'eps': row['eps'],
+             'climb': row['climb'],
+             'epc': row['epc'],
+             'ssid': row['essid'],
+             'bssid': row['apaddr'],
+             'signal': row['siglevel'],
+             'noise': row['nlevel'],
+             'quality': row['quality'],
+             'timestamp': row['time_aquired'],
+             'scn[]': scn
+         },True)
+        except UnicodeEncodeError:
+          log("Warning: UnicodeEncodeError.")
+          params = urllib.urlencode({
+             'lat': row['latitude'],
+             'long': row['longitude'],
+             'eph': row['eph'], 
+             'alt': row['altitude'],
+             'epv': row['epv'],
+             'track': row['track'],
+             'ept': row['ept'],
+             'speed': row['speed'],
+             'eps': row['eps'],
+             'climb': row['climb'],
+             'epc': row['epc'],
+             'ssid': row['essid'],
+             'bssid': row['apaddr'],
+             'signal': row['siglevel'],
+             'noise': row['nlevel'],
+             'quality': row['quality'],
+             'timestamp': row['time_aquired'],
+         },True)
+        conn.set_debuglevel(10)
         conn.request("POST", "/gps.php", params, headers)
-        response = conn.getresponse()
-        response.read()
+        sleep(3)
+        try:
+           response = conn.getresponse()
+        except:
+           log('httplib exception risen')
+           conn = httplib.HTTPConnection("q84fh.mydevil.net")
+           continue
+           pass
+        
         if response.status == 200:
             cur.execute("UPDATE POMIARY SET sent_timestamp = CURRENT_TIMESTAMP, sent = 1 WHERE id = ?;", [row['id']] )
             con.commit()
             send_counter = send_counter + 1
+        if send_counter % 10 == 0:
+            cur.execute("select count(*) as count from POMIARY WHERE SENT = 0 AND MODE = 3")
+            cur_count = cur.fetchone()['count']
+            
+            log("Sent %d/%d (%d nets). %d left, d: %d" % (send_counter,cnt,net_counter,cnt-send_counter,cur_count-new_count))
+            speed = (cur_count-new_count)/(time()-time_mark)
+            eta = ((send_counter - cnt)/speed)/60 
+            log("Speed: %f, eta: %d min." % (speed,eta))
+            net_counter = 0
+            time_mark = time()
+            new_count = cur_count
 except:
-    log("Error: Sending job completed: Brak polaczenia")
+    log("Error: Sending job failed: exception risen")
     conn.close()   
     os.unlink(pidfile)
-    raise 
+    print(row)
+    print(scn)
+    raise
+    #pass
     sys.exit()
+
 deleter = con.cursor()
+
+deleter.execute("delete from sieci where pomiar_gps in (select id from POMIARY where sent = 1")
+con.commit()
+del_sieci = deleter.rowcount
+
 deleter.execute("DELETE from POMIARY WHERE SENT = 1 AND time_aquired < date('now', '-1 days');")
 con.commit()
-log("Sending job completed: sent %s entries, deleted %d old rows in database" % (send_counter,deleter.rowcount))
+del_pomiary = deleter.rowcount
+
+log("Completed: sent %s entries, deleted %d/%d" % (send_counter,del_pomiary,del_sieci))
+
 con.close()
 conn.close()
 os.unlink(pidfile)
